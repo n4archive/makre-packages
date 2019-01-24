6dbf3e10223823b49201bad414a65d39928393ba
nift4 <36458596+nift4@users.noreply.github.com>
2019-01-24 13:51:05 +0100

    Update compiled packages. (build.sh)

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
+REPO="https://nift4.github.io/makre-packages/"
 rm -rdf tar-files/*
+MD="# Packages"
 for X in `cd src;find . -mindepth 1 -maxdepth 1 -type d;cd ..`;do
 	cd src/$X
 	tar Jcf ../../tar-files/${X}.tar *
+	MD="${MD}\n## ${X}\n### Description\nDESC\n### Website\n[Click](${REPO}/src/${X}/index.html)"
 	cd ../..
 done
+echo -e $MD > packages.md
 git add .
 git commit -m "Update compiled packages. (build.sh)"
 git push origin master
---
+++ packages.md
@@ -0,0 +1,11 @@
+# Packages
+## ./makefile
+### Description
+DESC
+### Website
+[Click](https://nift4.github.io/makre-packages//src/./makefile/index.html)
+## ./sh
+### Description
+DESC
+### Website
+[Click](https://nift4.github.io/makre-packages//src/./sh/index.html)
